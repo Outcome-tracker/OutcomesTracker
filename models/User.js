acc342b6c434065d9f4167cf8737ecceebe3f542
JesusAlonsosan~ <jesusalonsosan@gmail.com>
2019-04-23 11:46:40 -0500

    Models>user>creacion de Modelo de usuario

--- models/User.js
+++ models/User.js
@@ -1,13 +1,35 @@
+//Modelo de usuario
+
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    name: String,
+    username: String,
+
+    name:{
+      type: String,
+      required:true
+    },
+
+    lastname:{
+      type: String,
+    },
+
+    email:{
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    }
   },
 
   { timestamps: true }
 );
 
-module.exports = mongoose.model("user", carSchema);
+module.exports = mongoose.model("User", userSchema);
