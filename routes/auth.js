7b7aa23ffa53a596ab370487c1deaa2ef0556bc8
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-04-25 22:52:52 -0500

    Se añade register y el formulario recibe la data

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
 router.get("/login", (req, res) => {
   res.render("auth-form", {action: "Login"});
@@ -8,6 +9,16 @@ router.get("/login", (req, res) => {
 router.get("/register", (req, res) => {
   res.render("auth-form", {action: "Register"});
 })
+
+//Creacion de rutas para recibir la data y utilizamos passport local mongoose
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  /*User.registrer(req.body)
+  .then(user => {
+    console.log(user)
+  })*/
+});
 
 module.exports = router;
 
