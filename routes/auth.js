147ec99bda1735db240429bcd26cd715e21bfbd2
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-04-26 00:23:45 -0500

    me marca error POST /auth/resgister 404 86.088 ms - 1289

--- routes/auth.js
+++ routes/auth.js
@@ -3,21 +3,21 @@ const router = express.Router();
 const User = require("../models/User");
 
 router.get("/login", (req, res) => {
-  res.render("auth-form", {action: "Login"});
+  res.render("auth-form", { action: "Login"});
 });
 
 router.get("/register", (req, res) => {
-  res.render("auth-form", {action: "Register"});
+  res.render("auth-form", { action: "Register"});
 })
 
 //Creacion de rutas para recibir la data y utilizamos passport local mongoose
-
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  /*User.registrer(req.body)
-  .then(user => {
-    console.log(user)
-  })*/
+  User.register(req.body).then(user => {
+    console.log(user);
+  })
+  .catch(err => {
+    res.render("auth-form", { err });
+  }) 
 });
 
 module.exports = router;
