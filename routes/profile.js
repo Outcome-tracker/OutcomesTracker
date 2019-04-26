f40ee7cb760c9e8fecaf68a576d45b5f5f028e2c
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-04-26 18:13:17 -0500

    se crea profile js y hbs

---
+++ routes/profile.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router  = express.Router();
+
+/* para ver la vista del ususario  */
+
+const isAuth = (req,res,next) => {
+  if (req.isAuthenticated()) {
+    next ();
+  } else {
+    res.redirect("/login");
+  }
+};
+
+
+router.get("/",isAuth, (req, res) => {
+  res.render("profile");
+});
+
+
+module.exports = router;
