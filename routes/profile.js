08dafb12680bb5bf9e5a9ab40837589aade819aa
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-04-28 21:16:28 -0500

    :construction: Edicion de Perfil

--- routes/profile.js
+++ routes/profile.js
@@ -1,21 +1,33 @@
 const express = require("express");
-const router  = express.Router();
+const router = express.Router();
+const User = require("../models/User");
 
 
 /* para ver la vista del usuario  */
 
-const isAuth = (req,res,next) => {
+const isAuth = (req, res, next) => {
   if (req.isAuthenticated()) {
-    next ();
+    next();
   } else {
     res.redirect("/auth/login");
   }
 };
 
-router.get("/",isAuth, (req, res) => {
-  const { user }= req;
+router.get("/", isAuth, (req, res) => {
+  const { user } = req;
   res.render("profile", { user });
 });
 
+//ruta para profile-perfil del usuario
+router.get("/:id/edit",isAuth, (req, res) => {
+  const { id } = req.params;
+  User.findById(id)
+    .then(user => {
+      res.render("profile-form", { user });
+    })
+    .catch(err => {
+      res.render("profile-form", { err });
+    });
+});
 
 module.exports = router;
