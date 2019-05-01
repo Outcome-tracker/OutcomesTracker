9fd543a183aaa9b98c852bfe03d6c66ef3ce45c4
Rocio Perez <rocio.perez@ironhack.com>
2019-05-01 18:06:53 -0500

    student board & student form views

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-    res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/home', (req, res, next) => {
-    res.render('home');
+router.get("/home", (req, res, next) => {
+  res.render("home");
 });
 
-router.get('/student-board', (req, res, next) => {
-  res.render ('student-board');
+router.get("/student-board", (req, res, next) => {
+  res.render("student-board");
 });
 
-router.get('/sboard-form', (req, res, next) => {
-  res.render ('sboard-form');
+router.get("/sboard-form", (req, res, next) => {
+  res.render("sboard-form");
 });
-
 
 module.exports = router;
