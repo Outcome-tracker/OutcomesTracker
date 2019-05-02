ce01c4711fcc4a1e4b984bbd76f28c954bafb0f3
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-05-02 18:02:01 -0500

    fix try

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,5 @@ router.get('/', (req, res, next) => {
     res.render('index');
 });
 
-router.get('/home', (req, res, next) => {
-    res.render('home');
-});
-
-router.get('/student-board', (req, res, next) => {
-    res.render('student-board');
-});
-
-router.get('/sboard-form', (req, res, next) => {
-    res.render('sboard-form');
-});
 
 module.exports = router;
